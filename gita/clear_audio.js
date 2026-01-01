17b91d7b519585934de5e78ebe32125caabc3d1d
samrat kar <samratk@gmail.com>
2026-01-01 14:09:59 +0530

    files cleanup

--- gita/clear_audio.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('gita.json', 'utf8'));
-data.shlokas.forEach(s => s.audio = '');
-fs.writeFileSync('gita.json', JSON.stringify(data, null, 2), 'utf8');
-console.log('Cleared all audio URLs - users can now add their own');
