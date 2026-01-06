e48f21e479d30726e1e38468a5fb25f952536daa
samrat kar <samratk@gmail.com>
2026-01-06 21:44:57 +0530

    new yml fie

--- lavender-morning/vite.config.ts
+++ lavender-morning/vite.config.ts
@@ -19,6 +19,19 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      publicDir: 'data',
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: (assetInfo) => {
+              if (assetInfo.name === 'quotes.yaml') {
+                return 'data/[name][extname]';
+              }
+              return 'assets/[name]-[hash][extname]';
+            }
+          }
+        }
       }
     };
 });
